4761e9f9588ed2d304bf3703154e928cea52dd02
akshat250904 <girdharakshat5@gmail.com>
2025-02-25 17:03:38 +0530

    Update CORS origin to Vercel deployment URL

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin:"http://localhost:5173",
+  origin:"https://flare-p.vercel.app/login",
 }));
 app.use(fileUpload({
   useTempFiles: true,
